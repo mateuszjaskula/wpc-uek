d22ced45ee82a2f70794d2c09072583156951201
Mateusz Jaskula <jaskula92@gmail.com>
2018-05-12 09:15:49 +0000

    add template

--- web/app.py
+++ web/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello ():
-  return "It works"
+def hello():
+  return "It works Hurray!!!!"
+
+@app.route("/order-animation")
+def order_animation():
+  return render_template(
+    "order.html",
+    invitation="Welcome at ani creatior"
+  )
 
 
 if __name__ == '__main__':
